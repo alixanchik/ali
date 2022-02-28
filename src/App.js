9045de0bff592ab332440cc425cf010a2fa23646
a1ixanchik <kurmanbekovalihan81@gmail.com>
2022-02-28 16:11:15 +0300

    voda

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { fauser} from "@fortawesome/free-solid-svg-icons";
 
 
 function App() {
   return (
-    <div className="wrapper">
-  <svg/>
-  <div>
-    <h3>
-      REACT 
-    </h3>
-  </div>
+    <div className="wrapper clear">
+ <header className='d-flex justify-between'>
+   <div className="d-flex align-center">
+     <img width={40} height={40} src="/img/image 4.png"/>
+     <div>
+     <h3>
+     REACT SNEAKERS
+     </h3>
+     <p>
+     Магазин лучших кроссовок
+     </p>
+     </div>
+   </div>
+  <ul className="header2">
+    <li>
+    <img width={40} height={40} src="/img/Group.svg"/>
+    <span>1205 руб.</span>
 
+    </li>
+<li>
+<img width={60} height={40} src="/img/user.png"/>
+    </li>
+  </ul>
+  </header>
+<div className="sneaker">
+  <h1>
+Всем Кроссовки
+  </h1>
+.....
+</div>
     </div>
   );
 }
